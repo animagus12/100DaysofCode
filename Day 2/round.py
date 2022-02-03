53ad6387c708f409bdc9a040313f0795d485a430
Animagus <subhra.jit748@gmail.com>
2022-02-03 15:06:38 +0530

    Error Fixing

--- Day 2/round.py
+++ Day 2/round.py
@@ -1,18 +1,15 @@
-from turtle import heading
-
-
-print(8/3) # float
+print(8/3)  # float
 print(round(8/3, 2))
-print(8 // 3) # int
+print(8 // 3)  # int
 
 score = 4/2
-score //=2
+score //= 2
 print(score)
 
 # f-string
-score=0
-height =1.8
+score = 0
+height = 1.8
 isWinning = True
 
-print(f"your score is {score}, your height is {height}, you're winning is {isWinning}")
-
+print(
+    f"your score is {score}, your height is {height}, you're winning is {isWinning}")
