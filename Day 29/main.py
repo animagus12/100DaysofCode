43c3ad2e511d425c5b328f069250b159b56a362e
Animagus <subhra.jit748@gmail.com>
2022-02-24 15:14:50 +0530

    Day 29

---
+++ Day 29/main.py
@@ -0,0 +1,91 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+FONT = ("Calibri", 12, "bold")
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+def generator():
+    pass_entry.delete(0, END)
+    # Password Generator Project
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters =  [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols =  [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers =  [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def add_pass():
+    website = website_entry.get()
+    username = uname_entry.get()
+    password = pass_entry.get()
+
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+        messagebox.showerror(
+            title="Oops", message="Don't leave any fields empty")
+    else:
+
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"These are the detals enetered: \nWebsite: {website} \nEmail/Username: {username} \nPassword: {password} \nIs this okay to save?")
+
+        if is_ok == True:
+            with open("Python/#100DaysOfCode/Day 29/data.txt", "a") as f:
+                f.write(
+                    f"{website} | {username} | {password} \n")
+                website_entry.delete(0, END)
+                pass_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+root = Tk()
+root.title('Password Manager')
+root.config(padx=50, pady=100)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo = PhotoImage(file="Python/#100DaysOfCode/Day 29/logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_lbl = Label(root, text="Website:", font=FONT)
+website_lbl.grid(column=0, row=1)
+uname_lbl = Label(root, text="Email/Username:", font=FONT)
+uname_lbl.grid(column=0, row=2)
+pass_lbl = Label(root, text="Password:", font=FONT)
+pass_lbl.grid(column=0, row=3)
+
+# Entry Boxes
+website_entry = Entry(root, width=35)
+website_entry.grid(column=1, row=1, columnspan=2, sticky="ew")
+website_entry.focus()
+uname_entry = Entry(root, width=35)
+uname_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
+uname_entry.insert(0, "animagus@gmail.com")
+pass_entry = Entry(root, width=21)
+pass_entry.grid(column=1, row=3, sticky="ew")
+
+# Buttons
+gen_pass_btn = Button(root, text="Generate Password", font=FONT, command=generator)
+gen_pass_btn.grid(column=2, row=3, sticky="ew")
+add_btn = Button(root, text="Add", width=35, font=FONT, command=add_pass)
+add_btn.grid(column=1, row=4, columnspan=2, sticky="ew")
+
+root.mainloop()
