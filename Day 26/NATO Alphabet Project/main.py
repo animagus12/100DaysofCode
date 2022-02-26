5df900772b9643ef316db61455f4ea78819a784c
Animagus <subhra.jit748@gmail.com>
2022-02-26 09:59:11 +0530

    updated try except

--- Day 26/NATO Alphabet Project/main.py
+++ Day 26/NATO Alphabet Project/main.py
@@ -4,6 +4,14 @@ data= pandas.read_csv("Python/#100DaysOfCode/Day 26/NATO Alphabet Project/nato_p
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_word = input("Enter a word to convert to phonetic: ").upper()
-final_list = [phonetic_dict[user_letter] for user_letter in user_word if user_letter in phonetic_dict]
-print(final_list)
+def generator():
+    user_word = input("Enter a word to convert to phonetic: ").upper()
+    try:
+        final_list = [phonetic_dict[user_letter] for user_letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the word. Please!")
+        generator()
+    else:
+        print(final_list)
+
+generator()
