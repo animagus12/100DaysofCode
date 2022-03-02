543357305dc3c23814612920f856ba11131bc1f8
Animagus <subhra.jit748@gmail.com>
2022-03-02 16:15:31 +0530

    Day 35

--- Day 35/main.py
+++ Day 35/main.py
@@ -1,17 +1,38 @@
 import requests
+from twilio.rest import Client
 
 ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall'
-api_key = "f6c1586d81afb95c234599cc660f643b"
+api_key = 'f6c1586d81afb95c234599cc660f643b'
+
+account_sid = 'AC8a2f8b00db295548bc94aa23a7ffe34b'
+auth_token = '6a4dffcbc0d19b0fa18f871660633f58'
+
 parameters = {
-    "lat": 22.260424,
-    "lon": 84.853584,
-    "exclude": "current,minutely,daily",
-    "appid": api_key,
+    'lat': -19.433442,
+    # 22.260424,
+    'lon': 121.626892,
+    # 84.853584,
+    'exclude': 'current,minutely,daily',
+    'appid': api_key,
 }
 
-response = requests.get(
-    url=ENDPOINT, params=parameters)
+response = requests.get(url=ENDPOINT, params=parameters)
 response.raise_for_status()
 
-weather_data = response.json()
-print(weather_data)
+will_rain = False
+
+for i in range(0, 12):
+    weather_data = response.json()['hourly'][i]['weather'][0]['id']
+    if weather_data < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                    .create(
+                        body="It's going to rain today. Remember to bring your Umbrella!",
+                        from_='+19107271760',
+                        to='+919090305392'
+                    )
+
+    print(message.status)
