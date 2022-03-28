39493fff5b82354fbcc434d0e15729b414f6f21f
animaguss12 <subhra.jit748@gmail.com>
2022-03-28 17:24:40 +0530

    Day 57

---
+++ Day 57/Blog/main.py
@@ -0,0 +1,23 @@
+from urllib import response
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+BLOG_URL = 'https://api.npoint.io/c790b4d5cab58020d391'
+responses = requests.get(BLOG_URL).json()
+
+@app.route('/')
+def home():
+    return render_template("index.html", all_posts = responses)
+
+@app.route('/post/<int:index>')
+def post(index):
+    response = None
+    for response in responses:
+        if response["id"] == index:
+            requested_post = response
+    return render_template("post.html", post = requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
